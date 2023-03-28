cab9a02a0a8fa7f6d85ab2b8f19ee67f38f253fd
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-03-28 19:03:35 +0530

    Update font.py

--- plugins/modes.py/font.py
+++ plugins/modes.py/font.py
@@ -1,5 +1,5 @@
 import os
-from plugins.texts import Fonts
+from plugins.modes.texts import Fonts
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
