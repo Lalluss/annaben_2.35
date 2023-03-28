8304b6a03d8e04e937907a914057712f2782c39a
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-03-28 12:51:09 +0530

    🥲

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -18,6 +18,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink
 from database.users_chats_db import db
+from filterdb import Media, get_file_details, get_search_results
 from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
 from database.filters_mdb import (
     del_all,
