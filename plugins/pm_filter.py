61ace54eeb143bf48b2eb1e2120b55d1bd3acc2f
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-03-28 15:02:46 +0530

    Fix auto filter

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1307,7 +1307,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if 2 < len(message.text) < 100:
             search = message.text
-            files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
+            files, offset, total_results = await get_search_results(message.chat.id, search.lower(), offset=0, filter=True)
             if not files:
                 if SPELL_MODE:  
                     reply = search.replace(" ", "+")
