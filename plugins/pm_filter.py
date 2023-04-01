18fa10c13fade797d9dd5f6ecb1c02faf54f7de1
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-01 22:28:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -118,7 +118,7 @@ async def next_page(bot, query):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[{get_size(file.file_size)}] {file.file_name}", url=("https://telegram.me/{temp.U_NAME}?start=(callback_data=gtfl)"), await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                        text=f"[{get_size(file.file_size)}] {file.file_name}", url="https://telegram.me/{temp.U_NAME}?start=(callback_data=gtfl)", await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
                 for file in files
@@ -127,11 +127,11 @@ async def next_page(bot, query):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"{file.file_name}", url=("https://telegram.me/{temp.U_NAME}?start=(callback_data=gtfl)"), await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                        text=f"{file.file_name}", url="https://telegram.me/{temp.U_NAME}?start=(callback_data=gtfl)", await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                     InlineKeyboardButton(
                         text=f"{get_size(file.file_size)}",
-                        url=("https://telegram.me/{temp.U_NAME}?start=(callback_data=gtfl)"), await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                        url="https://telegram.me/{temp.U_NAME}?start=(callback_data=gtfl)", await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
                 for file in files
