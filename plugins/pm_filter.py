d98352667917b29f350bba0ea94e8e7b1dc487c1
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-02 15:19:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1155,7 +1155,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "astart":
         await query.message.delete()
         buttons = [[
-            InlineKeyboardButton('𝚂𝚃𝙰𝚁𝚃', url=f'https://t.me/{temp.U_NAME}?start=true'),
+            InlineKeyboardButton('𝚂𝚃𝙰𝚁𝚃', callback_data='start'),
             InlineKeyboardButton('𝙲𝙻𝙾𝚂𝙴', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
