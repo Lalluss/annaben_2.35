7c54ac3fbf188dc72d86d421d5ba127388daaa2f
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-03 18:39:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -42,6 +42,10 @@ SPELL_CHECK = {}
 SPELL_MODE = True
 
 SPELL_TXT = """➼ 𝑯𝒆𝒚 {mention}
+➣ Title: {title}
+➣ Genre: {genres}
+➣ Year: {year}
+➣ Rating: {rating}
 
 
 𝙲𝙷𝙴𝙲𝙺 𝚃𝙷𝙴 𝙸𝙽𝚂𝚃𝚁𝚄𝙲𝚃𝙸𝙾𝙽𝚂
@@ -1506,12 +1510,9 @@ async def auto_filter(client, msg, spoll=False):
                     ],[
                         InlineKeyboardButton("🔮ɪᴍᴅ🔮", url=f"https://imdb.com/find?q={reply}")
                     ]])
-                       await query.message.send_text(
-                             text=script.SPELL_TXT.format(query.from_user.mention),
-                             disable_web_page_preview=True,
-                             reply_markup=reply_markup,
-                             parse_mode=enums.ParseMode.HTML
-                       )
+                    imdb=await get_poster(search)
+                    if imdb and imdb.get('poster'):
+                        await message.reply_photo(photo=imdb.get('poster'), caption=LuciferMoringstar.GET_MOVIE_7.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), short=imdb.get('short_info'), url=imdb['url']), reply_markup=reply_markup) 
                         return
                     else:
                         return
