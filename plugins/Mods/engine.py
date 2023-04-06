2add359c1eddd123d86c7ea015e90baefa575ded
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-06 08:48:13 +0530

    Update engine.py

--- plugins/Mods/engine.py
+++ plugins/Mods/engine.py
@@ -1,7 +1,7 @@
 import openai
 
 async def ai(query):
-    openai.api_key = "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" #Your openai api key
+    openai.api_key = "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" #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
      
