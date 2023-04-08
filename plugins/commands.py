26ded8b45041c9557a66b385f6bd970818051bbd
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-08 11:27:19 +0530

    client

--- plugins/commands.py
+++ plugins/commands.py
@@ -286,7 +286,7 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                      [
-                      InlineKeyboardButton('Fast Download', url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")),
+                      InlineKeyboardButton('Fast Download', url=await get_shortlink(client.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")),
                       InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                    ],[
                       InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
@@ -339,7 +339,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
              [
-              InlineKeyboardButton('Fast Download', url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")),
+              InlineKeyboardButton('Fast Download', url=await get_shortlink(client.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")),
               InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
            ],[
               InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
