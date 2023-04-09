4823ae04f834dc6625bd083bb7368105642503fb
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-09 23:36:43 +0530

    Update write.py

--- plugins/Mods/write.py
+++ plugins/Mods/write.py
@@ -4,7 +4,7 @@ import requests
 
 
 @Client.on_message(filters.command("write"))
-async def handwriting(_, message):
+async def handwriting(client, message):
     if len(message.command) < 2:
         return await message.reply_text("» Give some text to write...")
     m = await message.reply_text("» I writing please wait...")
@@ -16,7 +16,7 @@ async def handwriting(_, message):
     API = "https://apis.xditya.me/write?text=" + name
     url = requests.get(API).url
     await m.edit("» Uploading...")
-    me = await bot.get_me()
+    me = await client.get_me()
     await message.reply_photo(url, caption=f"""**~ Request by {message.from_user.mention}**\n
 **~ Made by @lallu_tgs**""",
     reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton(text="Download Link", url=url)]]))
