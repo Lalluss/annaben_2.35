5b4f97a9322bf2dd2b50d5824778a49b8d99a987
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-09 23:33:01 +0530

    Create write.py

---
+++ plugins/Mods/write.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import *
+import requests 
+
+
+@Client.on_message(filters.command("write"))
+async def handwriting(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("» Give some text to write...")
+    m = await message.reply_text("» I writing please wait...")
+    name = (
+        message.text.split(None, 1)[1]
+        if len(message.command) < 3
+        else message.text.split(None, 1)[1].replace(" ", "%20")
+    )
+    API = "https://apis.xditya.me/write?text=" + name
+    url = requests.get(API).url
+    await m.edit("» Uploading...")
+    me = await bot.get_me()
+    await message.reply_photo(url, caption=f"""**~ Request by {message.from_user.mention}**\n
+**~ Made by @lallu_tgs**""",
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton(text="Download Link", url=url)]]))
