055bbcfc7efdc96f425bcd24cad041c475416e0a
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-10 10:47:26 +0530

    Adde new telegraph

---
+++ plugins/Mods/telegraph.py
@@ -0,0 +1,48 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
+from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
+from pyrogram import Client as app, telegraph, dev_user
+from telegraph import upload_file
+
+@app.on_message(filters.command("txt"))
+async def txt(_, message: Message):
+    reply = message.reply_to_message
+
+    if not reply or not reply.text:
+        return await message.reply("Reply to a text message")
+
+    if len(message.command) < 2:
+        return await message.reply("**Usage:**\n /txt [Page name]")
+
+    page_name = message.text.split(None, 1)[1]
+    page = telegraph.create_page(
+        page_name, html_content=(reply.text.html).replace("\n", "<br>")
+    )
+    return await message.reply(
+        f"**Posted:** {page['url']}",reply_markup=InlineKeyboardMarkup([ 
+        [InlineKeyboardButton('View 💫' , url=f"{page['url']}")]
+    ]),disable_web_page_preview=True,
+    )
+        
+
+@app.on_message(filters.command('tm'))
+def tm(_,message):
+    reply = message.reply_to_message
+    if not reply:
+          return message.reply_text("Reply to a **Media** to get a permanent telegra.ph link.")
+    if reply.text:
+          return message.reply_text("Reply to a **Media** to get a permanent telegra.ph link.")
+    msg = message.reply_text("downloading")
+    if reply.media:
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+           url = "https://telegra.ph" + x
+    msg.edit("uploading")
+    buttons = [[InlineKeyboardButton('View 💫' , url=f"{url}")]] 
+    if url.endswith("jpg"):
+             message.reply_photo(url,caption=f"{url}",reply_markup=InlineKeyboardMarkup(buttons))
+    elif url.endswith("mp4"):
+             message.reply_animation(url,caption=f"{url}",reply_markup=InlineKeyboardMarkup(buttons))
+    msg.delete()
