daf5560d116463916cb284b79d3fe6212b9c478e
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-10 19:03:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,6 +13,18 @@ def is_enabled(value, default):
     else:
         return default
 
+if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME'))
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
