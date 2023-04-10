c307fd1c65891f949a2e9c609fbdfa25d361e912
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-10 19:09:08 +0530

    Try to fix bug

---
+++ Lallus/vars.py
@@ -0,0 +1,27 @@
+
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+class Var(object):
+    APP_NAME = str(getenv('APP_NAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME'))
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
