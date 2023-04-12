edc0ed2304306b050f610bc2a4dc95d755ae696b
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-12 18:43:12 +0530

    Tnx jisin

--- server/stream_routes.py
+++ server/stream_routes.py
@@ -1,168 +1,95 @@
-
-import re
+# (c) adarsh-goel
 import time
 import math
 import logging
 import secrets
 import mimetypes
+from ..vars import Var
 from aiohttp import web
-from aiohttp.http_exceptions import BadStatusLine
 from pyrogram import Client
-from Lallus.exceptions import FIleNotFound, InvalidHash
-from pyrogram.types import StartTime, __version__
-from Lallus.utils.time_format import get_readable_time
-from Lallus.utils.custom_dl import ByteStreamer
+from Lallus.utils.custom_dl import TGCustomYield, chunk_size, offset_fix
 from Lallus.utils.render_template import render_page
-from Lallus.vars import Var
-
-
+from Lallus.utils.time_format import get_readable_time
+import time
+StartTime = time.time()
 routes = web.RouteTableDef()
+from urllib.parse import quote_plus
+kg18="ago"
+
+
 
 @routes.get("/", allow_head=True)
-async def root_route_handler(_):
-    return web.json_response(
-        {
-            "server_status": "running",
-            "uptime": get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + Client.username,
-            "connected_bots": len(multi_clients),
-            "loads": dict(
-                ("bot" + str(c + 1), l)
-                for c, (_, l) in enumerate(
-                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
-                )
-            ),
-            "version": __version__,
-            "owner": "jeol",
-        }
-    )
+async def root_route_handler(request):
+    return web.json_response({"status": "running",
+                              "maintained_by": "Adarsh Goel",
+                              "uptime": get_readable_time(time.time() - StartTime),
+                              "Bot was last updated": get_readable_time(time.time()),
+                              "ago":"",
+                              "telegram_bot": '@'+(await StreamBot.get_me()).username,
+                              "Bot Version":"3.0.1"})
 
 
-@routes.get(r"/watch/{path:\S+}", allow_head=True)
-async def stream_handler(request: web.Request):
+@routes.get("/watch/{message_id}")
+async def stream_handler(request):
     try:
-        path = request.match_info["path"]
-        match = re.search(r"^([a-zA-Z0-9_-]{6})(\d+)$", path)
-        if match:
-            secure_hash = match.group(1)
-            id = int(match.group(2))
-        else:
-            id = int(re.search(r"(\d+)(?:\/\S+)?", path).group(1))
-            secure_hash = request.rel_url.query.get("hash")
-        return web.Response(text=await render_page(id, secure_hash), content_type='text/html')
-    except InvalidHash as e:
-        raise web.HTTPForbidden(text=e.message)
-    except FIleNotFound as e:
-        raise web.HTTPNotFound(text=e.message)
-    except (AttributeError, BadStatusLine, ConnectionResetError):
-        pass
-    except Exception as e:
-        logging.critical(e.with_traceback(None))
-        raise web.HTTPInternalServerError(text=str(e))
-
-@routes.get(r"/{path:\S+}", allow_head=True)
-async def stream_handler(request: web.Request):
+        message_id = int(request.match_info['message_id'])
+        return web.Response(text=await render_page(message_id), content_type='text/html')
+    except ValueError as e:
+        logging.error(e)
+        raise web.HTTPNotFound
+        
+@routes.get("/download/{message_id}")
+@routes.get("/{message_id}")
+async def old_stream_handler(request):
     try:
-        path = request.match_info["path"]
-        match = re.search(r"^([a-zA-Z0-9_-]{6})(\d+)$", path)
-        if match:
-            secure_hash = match.group(1)
-            id = int(match.group(2))
-        else:
-            id = int(re.search(r"(\d+)(?:\/\S+)?", path).group(1))
-            secure_hash = request.rel_url.query.get("hash")
-        return await media_streamer(request, id, secure_hash)
-    except InvalidHash as e:
-        raise web.HTTPForbidden(text=e.message)
-    except FIleNotFound as e:
-        raise web.HTTPNotFound(text=e.message)
-    except (AttributeError, BadStatusLine, ConnectionResetError):
-        pass
-    except Exception as e:
-        logging.critical(e.with_traceback(None))
-        raise web.HTTPInternalServerError(text=str(e))
+        message_id = int(request.match_info['message_id'])
+        return await media_streamer(request, message_id)
+    except ValueError as e:
+        logging.error(e)
+        raise web.HTTPNotFound
+        
 
-class_cache = {}
-
-async def media_streamer(request: web.Request, id: int, secure_hash: str):
-    range_header = request.headers.get("Range", 0)
-    
-    index = min(work_loads, key=work_loads.get)
-    faster_client = multi_clients[index]
-    
-    if Var.MULTI_CLIENT:
-        logging.info(f"Client {index} is now serving {request.remote}")
-
-    if faster_client in class_cache:
-        tg_connect = class_cache[faster_client]
-        logging.debug(f"Using cached ByteStreamer object for client {index}")
-    else:
-        logging.debug(f"Creating new ByteStreamer object for client {index}")
-        tg_connect = ByteStreamer(faster_client)
-        class_cache[faster_client] = tg_connect
-    logging.debug("before calling get_file_properties")
-    file_id = await tg_connect.get_file_properties(id)
-    logging.debug("after calling get_file_properties")
-    
-    if file_id.unique_id[:6] != secure_hash:
-        logging.debug(f"Invalid hash for message with ID {id}")
-        raise InvalidHash
-    
-    file_size = file_id.file_size
+async def media_streamer(request, message_id: int):
+    range_header = request.headers.get('Range', 0)
+    media_msg = await Client.get_messages(Var.BIN_CHANNEL, message_id)
+    file_properties = await TGCustomYield().generate_file_properties(media_msg)
+    file_size = file_properties.file_size
 
     if range_header:
-        from_bytes, until_bytes = range_header.replace("bytes=", "").split("-")
+        from_bytes, until_bytes = range_header.replace('bytes=', '').split('-')
         from_bytes = int(from_bytes)
         until_bytes = int(until_bytes) if until_bytes else file_size - 1
     else:
         from_bytes = request.http_range.start or 0
-        until_bytes = (request.http_range.stop or file_size) - 1
+        until_bytes = request.http_range.stop or file_size - 1
 
-    if (until_bytes > file_size) or (from_bytes < 0) or (until_bytes < from_bytes):
-        return web.Response(
-            status=416,
-            body="416: Range not satisfiable",
-            headers={"Content-Range": f"bytes */{file_size}"},
-        )
+    req_length = until_bytes - from_bytes
 
-    chunk_size = 1024 * 1024
-    until_bytes = min(until_bytes, file_size - 1)
-
-    offset = from_bytes - (from_bytes % chunk_size)
+    new_chunk_size = await chunk_size(req_length)
+    offset = await offset_fix(from_bytes, new_chunk_size)
     first_part_cut = from_bytes - offset
-    last_part_cut = until_bytes % chunk_size + 1
+    last_part_cut = (until_bytes % new_chunk_size) + 1
+    part_count = math.ceil(req_length / new_chunk_size)
+    body = TGCustomYield().yield_file(media_msg, offset, first_part_cut, last_part_cut, part_count,
+                                      new_chunk_size)
 
-    req_length = until_bytes - from_bytes + 1
-    part_count = math.ceil(until_bytes / chunk_size) - math.floor(offset / chunk_size)
-    body = tg_connect.yield_file(
-        file_id, index, offset, first_part_cut, last_part_cut, part_count, chunk_size
-    )
+    file_name = file_properties.file_name if file_properties.file_name \
+        else f"{secrets.token_hex(2)}.jpeg"
+    mime_type = file_properties.mime_type if file_properties.mime_type \
+        else f"{mimetypes.guess_type(file_name)}"
 
-    mime_type = file_id.mime_type
-    file_name = file_id.file_name
-    disposition = "attachment"
-
-    if mime_type:
-        if not file_name:
-            try:
-                file_name = f"{secrets.token_hex(2)}.{mime_type.split('/')[1]}"
-            except (IndexError, AttributeError):
-                file_name = f"{secrets.token_hex(2)}.unknown"
-    else:
-        if file_name:
-            mime_type = mimetypes.guess_type(file_id.file_name)
-        else:
-            mime_type = "application/octet-stream"
-            file_name = f"{secrets.token_hex(2)}.unknown"
-
-    return web.Response(
+    return_resp = web.Response(
         status=206 if range_header else 200,
         body=body,
         headers={
-            "Content-Type": f"{mime_type}",
+            "Content-Type": mime_type,
             "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
-            "Content-Length": str(req_length),
-            "Content-Disposition": f'{disposition}; filename="{file_name}"',
+            "Content-Disposition": f'attachment; filename="{file_name}"',
             "Accept-Ranges": "bytes",
-        },
+        }
     )
+
+    if return_resp.status == 200:
+        return_resp.headers.add("Content-Length", str(file_size))
+
+    return return_resp
