c92e20d8af9f76fcc22d6af66b0f0f24688406be
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-19 09:17:39 +0530

    Fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1765,7 +1765,6 @@ async def auto_filter(client, msg, spoll=False):
         grpid = await active_connection(str(message.from_user.id))
         await save_group_settings(grpid, 'auto_delete', True)
         settings = await get_settings(message.chat.id)
-        if settings['auto_delete']:
 
     btn.insert(0, [
         InlineKeyboardButton("Send All !", callback_data=f"send_fall#{pre}#{message.chat.id}-{message.id}#{0}"),
