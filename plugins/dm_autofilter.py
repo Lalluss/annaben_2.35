2d4deaaa3cb14eadfe1850f557acfbf5fb929c17
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-19 12:01:33 +0530

    Update dm_autofilter.py

--- plugins/dm_autofilter.py
+++ plugins/dm_autofilter.py
@@ -73,7 +73,7 @@ async def pm_next_page(bot, query):
         else:
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
-     else:        
+    else:        
         if SINGLE_BUTTON:   
             btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
         else:
