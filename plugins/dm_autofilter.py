825548d9b6b4590095cbd409ca03185a3b05e2b7
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-19 10:29:28 +0530

    Update dm_autofilter.py

--- plugins/dm_autofilter.py
+++ plugins/dm_autofilter.py
@@ -7,7 +7,7 @@ import pyrogram
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 from utils import get_shortlink 
-from info import AUTH_USERS, PM_IMDB, SINGLE_BUTTON, PROTECT_CONTENT, SPELL_CHECK_REPLY, IMDB_TEMPLATE, IMDB_DELET_TIME, G_FILTER, SHORT_URL, SHORT_API, PMFILTER
+from info import AUTH_USERS, PM_IMDB, SINGLE_BUTTON, PROTECT_CONTENT, SPELL_CHECK_REPLY, IMDB_TEMPLATE, IMDB_DELET_TIME, G_FILTER PMFILTER
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters, enums 
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
