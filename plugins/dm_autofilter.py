7807b096a0a57ffd73532e69c332a6451c882121
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-19 11:54:00 +0530

    Update dm_autofilter.py

--- plugins/dm_autofilter.py
+++ plugins/dm_autofilter.py
@@ -153,7 +153,8 @@ async def auto_filter(client, msg, spoll=False):
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = pmspoll
     pre = 'pmfilep' if PROTECT_CONTENT else 'pmfile'
-
+    else:        
+        if SINGLE_BUTTON:
             btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}')] for file in files ]
         else:
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'{pre}#{req}#{file.file_id}'),
